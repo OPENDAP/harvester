56d9eb31cae717d0b52345b4b819ad3160b163d2
grined <grinchenko.ed@gmail.com>
2016-05-13 10:37:35 +0300

    Add support of Http and Https together

--- collector/src/main/java/org/opendap/harvester/HarvesterApplication.java
+++ collector/src/main/java/org/opendap/harvester/HarvesterApplication.java
@@ -5,15 +5,42 @@
  */
 package org.opendap.harvester;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.SocketUtils;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableMongoRepositories
 public class HarvesterApplication {
+/*
+	 Enable this if you want to have both http and https connectors in embedded tomcat by
+	 Spring Boot
+ */
+//	@Bean
+//	public Integer port() {
+//		return SocketUtils.findAvailableTcpPort();
+//	}
+//
+//	@Bean
+//	public EmbeddedServletContainerFactory servletContainer() {
+//		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//		return tomcat;
+//	}
+//
+//	private Connector createStandardConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setPort(port());
+//		return connector;
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HarvesterApplication.class, args);
 	}
