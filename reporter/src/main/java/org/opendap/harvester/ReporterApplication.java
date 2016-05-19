0162b5f2754d64a3f319ca5ffdb4a56b46cfae0a
grined <grinchenko.ed@gmail.com>
2016-05-19 14:04:29 +0300

    AL-13 Added logging to the reporter

--- reporter/src/main/java/org/opendap/harvester/ReporterApplication.java
+++ reporter/src/main/java/org/opendap/harvester/ReporterApplication.java
@@ -5,6 +5,8 @@
  */
 package org.opendap.harvester;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -19,7 +21,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class ReporterApplication extends SpringBootServletInitializer {
-
+	private static final Logger log = LoggerFactory.getLogger(ReporterApplication.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -28,5 +30,6 @@ public class ReporterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReporterApplication.class, args);
+		log.info("Application has been started");
 	}
 }
