d336614aeeb7b9b3b3ea14f703f15f0d9d10bf13
grined <grinchenko.ed@gmail.com>
2016-08-14 23:41:02 +0300

    AL-19 Error page(s)

--- reporter/src/main/java/org/opendap/harvester/controller/CustomErrorController.java
+++ reporter/src/main/java/org/opendap/harvester/controller/CustomErrorController.java
@@ -1,23 +1,42 @@
 package org.opendap.harvester.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 @Controller
 public class CustomErrorController implements ErrorController {
     private static final String PATH = "/error";
+    @Autowired
+    private ErrorAttributes errorAttributes;
 
     @RequestMapping(value = PATH)
-    public String error(HttpServletRequest request, HttpServletResponse response) {
-        return "404.html";
+    public String error(Model model, HttpServletRequest request) {
+        Map<String, Object> errorAttributes = getErrorAttributes(request, true);
+        model.addAttribute("status", errorAttributes.get("status"));
+        model.addAttribute("error", errorAttributes.get("error"));
+        model.addAttribute("message", errorAttributes.get("message"));
+        String stacktrace = (String) errorAttributes.get("trace");
+        String[] split = stacktrace.split("\\r\\n");
+        model.addAttribute("trace", split);
+        return "errorPage";
     }
 
     @Override
     public String getErrorPath() {
         return PATH;
+    }
+
+    private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
     }
 }
